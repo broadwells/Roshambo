47bc5b035f4b2fb5f569192730032e11f6af11a3
broadwells <broadwell.s@gmail.com>
2017-04-24 13:09:16 -0400

    Track wins/losses added

--- src/RoshamboApp.java
+++ src/RoshamboApp.java
@@ -14,6 +14,8 @@ public class RoshamboApp {
         System.out.println("Please enter your name");
         String player = scan.nextLine();
         HumanPlayer player1 = new HumanPlayer(player);
+        ShearGenius player2 = new ShearGenius("ShearGenius");
+        Rocky player3 = new Rocky("Rocky");
 
         boolean valid = false;  //validating the user enters only 'Rocky' or 'ShearGenius'
         while (!valid) {
@@ -27,6 +29,12 @@ public class RoshamboApp {
         boolean validInput = false;
         boolean startOver = true;
         String playChoice = "";
+        String result;
+        int ties = 0;
+        int win1 = 0;
+        int win2 = 0;
+        int loss1 = 0;
+        int loss2 = 0;
         while (startOver) {
             while (!validInput) {   //validating the user enters only the selections listed
                 System.out.println("Rock, paper, or scissors? (rock/paper/scissors): ");
@@ -35,9 +43,6 @@ public class RoshamboApp {
                 validInput = checkAnswer.gamePieceValid(playChoice);
             }
             if (opponent.equalsIgnoreCase("rocky")){
-
-                Rocky player3 = new Rocky("Rocky");
-
                 //declaring the Roshambo result to human and rocky to pass to winner method
                 String human = player1.generateRoshambo(playChoice);
                 String rocky = player3.generateRoshambo();
@@ -45,53 +50,74 @@ public class RoshamboApp {
                 System.out.println(player1.getPlayerName() + ": " + human.toLowerCase());
                 System.out.println(player3.getPlayerName() + ": " + rocky.toLowerCase());
 
-                String result = winner(human, rocky);   //prints out the result of the match
+                result = winner(human, rocky);   //prints out the result of the match
                 if (result.equals("draw")) {
                     System.out.println("Draw!");
+                    ties += 1;
                 }
                 else if (result.equals("p1")) {
                     System.out.println(player1.getPlayerName() + " wins!");
+                    win1 += 1;
+                    loss1 += 1;
                 }
                 else if (result.equals("p2")) {
                     System.out.println(player3.getPlayerName() + " wins!");
+                    win2 += 1;
+                    loss2 += 1;
                 }
 
             }
             else if (opponent.equalsIgnoreCase("sheargenius")){
-
-                ShearGenius player2 = new ShearGenius("ShearGenius");
-
                 String human = player1.generateRoshambo(playChoice);
                 String shearGenius = player2.generateRoshambo();
 
                 System.out.println(player1.getPlayerName() + ": " + human.toLowerCase());
                 System.out.println(player2.getPlayerName() + ": " + shearGenius.toLowerCase());
 
-                String result = winner(human, shearGenius);
+                result = winner(human, shearGenius);
                 switch (result) {
                     case "draw":
                         System.out.println("Draw!");
+                        ties += 1;
                         break;
                     case "p1":
                         System.out.println(player1.getPlayerName() + " wins!");
+                        win1 += 1;
+                        loss1 += 1;
                         break;
                     case "p2":
                         System.out.println(player2.getPlayerName() + " wins!");
+                        win2 += 1;
+                        loss2 += 1;
                         break;
                 }
+
             }
 
             System.out.println("Play again? (y/n): ");
             String again = scan.nextLine();
             again.toLowerCase();
             if (again.contains("n")) {
-                System.out.println("Thanks for playing, " + player + "!");
+                System.out.println("Thanks for playing " + player + "!");
                 startOver = false;
             }
             else {
                 validInput = false;
             }
 
+            //keeping score of wins/losses and ties
+            System.out.println(player1.getPlayerName() + " has won " + win1 + " matches, and lost " + loss2 + " " +
+                    "matches!");
+            if (opponent.equalsIgnoreCase("sheargenius")) {
+                System.out.println(player2.getPlayerName() + " has won " + win2 + " matches, and lost " + loss1 + " " +
+                        "matches!");
+            }
+            else if (opponent.equalsIgnoreCase("rocky")){
+                System.out.println(player3.getPlayerName() + " has won " + win2 + " matches, and lost " + loss1 + " " +
+                        "matches!");
+            }
+            System.out.println(ties + " ties!");
+            System.out.println();
         }
 
     }
